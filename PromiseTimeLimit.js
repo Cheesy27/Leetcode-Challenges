076c15e88b9df47e179d54e9f6c3b1bb91cca91a
Cheesy27 <lucasjan27@gmail.com>
2023-09-28 15:58:20 -0400

    Complete 2 new challanges

---
+++ PromiseTimeLimit.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+	return async function(...args) {
+        let funcExec = fn(...args);
+        let funcRej = new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+                reject("Time Limit Exceeded");
+            }, t)
+        })
+        return Promise.race([funcExec, funcRej])
+    }
+};
---
+++ intervalCancallation2725.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    let timer = setInterval(()=>fn(...args),t)
+    let cancel = ()=>clearInterval(timer)
+    return cancel
+};
